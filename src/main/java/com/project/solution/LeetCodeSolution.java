89b723f46cdd05a75ccf8257c95c061caa77597d
Emerson Millard <emillard.code@gmail.com>
2022-08-10 12:00:00 -0700

    Added solution classes.

---
+++ src/main/java/com/project/solution/LeetCodeSolution.java
@@ -0,0 +1,18 @@
+package com.project.solution;
+
+import com.project.solution.MyCalendar;
+
+public class LeetCodeSolution {
+
+    public static void main(String[] args) {
+
+        MyCalendar calender = new MyCalendar();
+
+        System.out.println(calender.book(10, 20));
+        System.out.println(calender.book(22, 28));
+        System.out.println(calender.book(5, 12));
+        System.out.println(calender.book(28, 30));
+
+    }
+
+}
