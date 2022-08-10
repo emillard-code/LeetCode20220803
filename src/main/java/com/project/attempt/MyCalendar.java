0279184b54caed895937bbb643d3b0bba65566db
Emerson Millard <emillard.code@gmail.com>
2022-08-10 12:00:00 -0700

    Added attempt class.

---
+++ src/main/java/com/project/attempt/MyCalendar.java
@@ -0,0 +1,16 @@
+package com.project.attempt;
+
+public class MyCalendar {
+
+    public MyCalendar() {
+
+    }
+
+    public boolean book(int start, int end) {
+
+
+        return true;
+
+    }
+
+}
