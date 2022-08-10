99b0ea06af1fb06a49ba6038ca88fcecacf9a0c0
Emerson Millard <emillard.code@gmail.com>
2022-08-10 12:00:00 -0700

    Modified attempt class.

--- src/main/java/com/project/attempt/LeetCodeApp.java
+++ src/main/java/com/project/attempt/LeetCodeApp.java
@@ -4,6 +4,13 @@ public class LeetCodeApp {
 
     public static void main(String[] args) {
 
+        MyCalendar calender = new MyCalendar();
+
+        System.out.println(calender.book(10, 20));
+        System.out.println(calender.book(22, 28));
+        System.out.println(calender.book(5, 12));
+        System.out.println(calender.book(28, 30));
+
     }
 
 }
