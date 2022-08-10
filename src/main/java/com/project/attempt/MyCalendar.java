9ac2a0cc87049f2062fc8e7153bce69d4f688725
Emerson Millard <emillard.code@gmail.com>
2022-08-10 12:00:00 -0700

    Modified attempt class.

--- src/main/java/com/project/attempt/MyCalendar.java
+++ src/main/java/com/project/attempt/MyCalendar.java
@@ -1,6 +1,11 @@
 package com.project.attempt;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MyCalendar {
+
+    private Set<Integer> dateSet = new HashSet<Integer>();
 
     public MyCalendar() {
 
@@ -8,6 +13,15 @@ public class MyCalendar {
 
     public boolean book(int start, int end) {
 
+        for (int i = start; i < end; i++) {
+
+            if (dateSet.contains(i)) {
+                return false;
+            } else {
+                dateSet.add(i);
+            }
+
+        }
 
         return true;
 
